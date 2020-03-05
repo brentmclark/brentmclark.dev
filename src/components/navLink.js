06f5e34b6af5541e6ff5e0585106c353c26e3e65
kigningcole <43896019+kigningcole@users.noreply.github.com>
2020-03-06 00:40:53 +0100

    increase nav link font size

--- src/components/navLink.js
+++ src/components/navLink.js
@@ -2,7 +2,7 @@
   import {Link} from 'gatsby'
 
 const NavLink = ({ to, children }) => {
-  return <Link className={`hover:text-gray-200 w-full block m-0 text-xs md:text-sm lg:text-md tracking-wider text-white`} to={to}>{children}</Link>
+  return <Link className={`hover:text-gray-200 w-full block m-0 text-sm md:text-base lg:text-md tracking-wider text-white`} to={to}>{children}</Link>
 }
 
 export default NavLink

2f0ccbee94045af67191c422088a81a65a5449ce
kigningcole <43896019+kigningcole@users.noreply.github.com>
2020-03-07 00:24:42 +0100

    small form fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,15 +10,15 @@ const ContactPage = () => {
   const [displayMessage, setDisplayMessage] = useState(null)
   const [messageType, setMessageType] = useState(null)
 
-  const [email, setEmail] = useState('')
-  const [fullname, setFullname] = useState('')
-  const [message, setMessage] = useState('')
+  const [email, setEmail] = useState("")
+  const [fullname, setFullname] = useState("")
+  const [message, setMessage] = useState("")
 
-  const button_background_class = disableButton === true ? 'bg-gray-600' : 'bg-blue-600'
-  const button_hover_class = disableButton === true ? '' : 'hover:bg-blue-700'
-  const button_cursor_class = disableButton === true ? 'cursor-not-allowed' : 'cursor-pointer'
+  const button_background_class = disableButton === true ? "bg-gray-600" : "bg-blue-600"
+  const button_hover_class = disableButton === true ? "" : "hover:bg-blue-700"
+  const button_cursor_class = disableButton === true ? "cursor-not-allowed" : "cursor-pointer"
 
-  const message_color_class = messageType === 'success' ? 'text-green-600' : 'text-red-600'
+  const message_color_class = messageType === "success" ? "text-green-600" : "text-red-600"
 
 
   const handleEmailSend = (e) => {
@@ -28,24 +28,24 @@ const ContactPage = () => {
 
     const SERVICE_ID = `brent_blog`
     const TEMPLATE_ID = `contact_form`
-    const USER_ID = `user_IzkVc56lW2umPWjkS01p2`;
+    const USER_ID = `user_IzkVc56lW2umPWjkS01p2`
 
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
       .then((result) => {
         console.log(result.text)
 
         setDisableButton(false)
-        setDisplayMessage('Your message was sent successfully and I will get back as soon as possible')
-        setMessageType('success')
-        setMessage('')
-        setEmail('')
-        setFullname('')
+        setDisplayMessage("Your message was sent successfully and I will get back as soon as possible")
+        setMessageType("success")
+        setMessage("")
+        setEmail("")
+        setFullname("")
       }, (error) => {
         console.log(error.text)
 
         setDisableButton(false)
-        setDisplayMessage('An error occcured. Please try again.')
-        setMessageType('error')
+        setDisplayMessage("An error occcured. Please try again.")
+        setMessageType("error")
       })
   }
 
@@ -60,13 +60,15 @@ const ContactPage = () => {
         <form className="w-full h-auto mt-6" onSubmit={handleEmailSend}>
           <input type="text" className="p-2 bg-white w-full font-semibold mb-6" placeholder={`Full Name (required)`}
                  name={`fullname`} value={email} onChange={e => setEmail(e.target.value)} required/>
-          <input type="email" className="p-2 bg-white w-full font-semibold mb-6" placeholder={`Email (required)`} name={`email`} value={fullname} onChange={e => setFullname(e.target.value)} required/>
-          <textarea name="" id="" cols="30" rows="8" className="w-full bg-white p-2 font-semibold mb-6"
-                    placeholder={`Message (required)`} value={message} onChange={e => setMessage(e.target.value)} required></textarea>
+          <input type="email" className="p-2 bg-white w-full font-semibold mb-6" placeholder={`Email (required)`}
+                 name={`email`} value={fullname} onChange={e => setFullname(e.target.value)} required/>
+          <textarea name="message" id="" cols="30" rows="8" className="w-full bg-white p-2 font-semibold mb-6"
+                    placeholder={`Message (required)`} value={message} onChange={e => setMessage(e.target.value)}
+                    required></textarea>
           {displayMessage && <p className={`w-full mb-6 ${message_color_class} font-semibold`}>{displayMessage}</p>}
           <button
             className={`text-white rounded py-2 ${button_background_class} ${button_hover_class} ${button_cursor_class} w-full font-semibold opacity-75 md:w-40`}
-            name={`message`}>Send Message
+          >Send Message
           </button>
         </form>
       </section>

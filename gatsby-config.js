8541192a51ffff74b6d36e89632583d1c342956d
Cole <emeruchecole9@gmail.com>
2020-03-20 10:11:53 +0100

    fixed error in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,13 +28,13 @@ module.exports = {
         name: `blog`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     path: `${__dirname}/content/assets`,
+    //     name: `assets`,
+    //   },
+    // },
     {
       resolve: `gatsby-transformer-remark`,
       options: {

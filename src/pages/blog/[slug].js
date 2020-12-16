12bd470433be73cdfae98f04ffc3644132211e73
Brent M Clark <brentmclark@gmail.com>
2020-12-16 09:33:30 -0600

    get blog posts working again

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -5,22 +5,78 @@ import React from "react"
 import { useRouter } from "next/router"
 import renderToString from "next-mdx-remote/render-to-string"
 import hydrate from "next-mdx-remote/hydrate"
+import matter from 'gray-matter'
+import { MDXProvider } from '@mdx-js/react'
 
-import Layout from "../../components/layout"
-import PageWrapper from '../../components/PageWrapper'
+import Layout from "components/layout"
+import PageWrapper from 'components/PageWrapper'
+import SEO from "components/seo"
+import SyntaxHighlighter from "components/SyntaxHighlighter"
+
+
+const components = {
+  pre: props => <div {...props} />,
+  code: SyntaxHighlighter,
+  inlineCode: props => <code style={{background: '#f5f2f0', padding: '.1em'}} {...props} />
+}
 
 const Post = props => {
-  const { source } = props
-  if (source == null) {
+  const { body, frontMatter } = props
+  if (body == null) {
     return <div>Loading</div>
   }
   const router = useRouter()
-  const { slug } = router.query
-  const content = hydrate(source)
+  const content = hydrate(body)
+  const formattedArticleDate = new Date(frontMatter.date).toLocaleString([], {dateStyle: 'long', timeStyle: 'short'})
   return (
-    <Layout>
+    <Layout location={router.location} title={`${frontMatter.title} :: Brent M. Clark`} id="blog-container">
+      <SEO
+        title={frontMatter.title}
+        description={frontMatter.description || frontMatter.excerpt}
+      />
       <PageWrapper>
-        {content}
+        <article>
+          <header>
+            <small className={`font-bold opacity-75 text-gray-700 my-2`}>
+              {formattedArticleDate}
+            </small>
+            <h1 className={`text-5xl font-semibold leading-snug text-blue-600`}>
+              {frontMatter.title}
+            </h1>
+          </header>
+          <MDXProvider components={components}>
+            <main className={`prose leading-relaxed mt-10`}>
+              {content}
+            </main>
+          </MDXProvider>
+        </article>
+
+        {/* <nav className={`mt-4`}>
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav> */}
       </PageWrapper>
     </Layout>
   )
@@ -38,13 +94,13 @@ async function getStaticProps(context) {
 
   const postPath = path.join(
     process.cwd(),
-    `src/posts/${params.slug}/index.mdx`
+    `src/posts/${params.slug}.mdx`
   )
   const post = fs.readFileSync(postPath)
-
-  const mdxSource = await renderToString(post)
+  const {content, data} = matter(post)
+  const mdxSource = await renderToString(content)
   return {
-    props: { source: mdxSource },
+    props: { body: mdxSource, frontMatter: data },
   }
 }
 

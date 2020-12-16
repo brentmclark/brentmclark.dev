2a3deb5b42d4bff16b9530b542c34549c68ca5ba
Brent M Clark <brentmclark@gmail.com>
2020-12-16 09:34:06 -0600

    fix link classes

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,10 +2,9 @@ import React from "react"
 import NextLink from "next/link"
 
 const Link = ({ to, children, className }) => {
-  console.log({ to })
   return (
-    <NextLink className={className} href={to}>
-      {children}
+    <NextLink href={to}>
+      <a className={className}>{children}</a>
     </NextLink>
   )
 }

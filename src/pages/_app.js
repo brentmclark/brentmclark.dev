46066c5e61e64f92c5284981e6e5ddcdc3d0794c
Brent M Clark <brentmclark@gmail.com>
2020-12-19 15:48:33 -0600

    favicons and manifest

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,31 @@
+import Head from 'next/head';
+
 import "../styles/global.css"
+
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link key="1" rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link key="2" rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link key="3" rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link key="4" rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link key="5" rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link key="6" rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link key="7" rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link key="8" rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link key="9" rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link key="10" rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+        <link key="11" rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link key="12" rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link key="13" rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link key="14" rel="manifest" href="/manifest.json" />
+        <meta key="15" name="msapplication-TileColor" content="#ffffff" />
+        <meta key="16" name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta key="17" name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }

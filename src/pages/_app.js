45fa80fa1310ba8625fa19953f9aac8901c4099a
Brent M Clark <brentmclark@gmail.com>
2021-01-19 22:56:45 -0600

    add GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,17 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* <!-- Google Tag Manager --> */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MHHQQG7');
+          `
+        }} />
+        {/* <!-- End Google Tag Manager --> */}
         <link
           key="1"
           rel="apple-touch-icon"
@@ -97,6 +108,7 @@ export default function MyApp({ Component, pageProps }) {
         />
         <meta key="17" name="theme-color" content="#ffffff" />
       </Head>
+      
       <Component {...pageProps} />
     </>
   )

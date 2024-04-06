721aa56b07d71880774d191ecf25a250f2519f77
Brent Clark <brent@backboneiq.com>
2024-04-05 19:07:06 -0500

    update to newest export process

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 
 module.exports = {
+  output: 'export',
   webpack: (config, options) => {
     config.resolve.alias.components = path.resolve("./src/components")
     return config
